e950d2b3c3debe7574263ec33dd1e124120b4aae
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-08-03 19:52:11 +0800

    Return origin string when vowels are less than thirty percent.

--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MommifierTest {
+
+    @Test
+    void should_return_origin_string_when_vowels_are_less_than_thirty_percent() {
+        Mommifier mommifier = new Mommifier();
+        String origin = "This is a test string";
+        String expected = mommifier.convert(origin);
+        assertEquals(expected, origin);
+    }
 }
